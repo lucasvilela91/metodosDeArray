b14de5f5dc9a2a0c4d6453612e299203fd99d6b3
Lucas F Vilela <lucasvilelapersonal@gmail.com>
2024-11-01 17:31:17 -0300

    Second Commit

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -9,6 +9,9 @@ botoes.forEach((btn) => btn.addEventListener('click', filtrarLivros));
 function filtrarLivros() {
   const elementoBtn = document.getElementById(this.id);
   const categoria = elementoBtn.value;
-  let livrosFiltrados = livros.filter((livro) => livro.categoria == categoria);
-  console.table(livrosFiltrados);
+  let livrosFiltrados =
+    categoria == 'disponivel'
+      ? livros.filter((livro) => livro.quantidade > 0)
+      : livros.filter((livro) => livro.categoria == categoria);
+  exibirOsLivrosNaTela(livrosFiltrados);
 }
